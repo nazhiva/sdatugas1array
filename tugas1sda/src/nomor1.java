fdba3f5ec499e910d5f4c11892129429bc211a2c
Nazhiva <100433664+nazhiva@users.noreply.github.com>
2022-02-25 09:12:00 -0800

    Update nomor1.java

--- tugas1sda/src/nomor1.java
+++ tugas1sda/src/nomor1.java
@@ -1,13 +1,14 @@
+//Soal no 1
+//Tentukan hasil yang didapatkan ketika dilakukan perintah berikut ini:
+//isEmpty() -> Jika tidak ada isi maka true, jika ada isi maka false
+
 public class nomor1 {
     public static void main(String[] args) {
         String[] namaku= {"I","T","R","I"};
-        //Soal no 1
-        //Tentukan hasil yang didapatkan ketika dilakukan perintah berikut ini:
-        //isEmpty() -> Jika tidak ada isi maka true, jika ada isi maka false
-
         System.out.println(namaku[0].isEmpty());     
         System.out.println(namaku[1].isEmpty());     
         System.out.println(namaku[2].isEmpty());     
         System.out.println(namaku[3].isEmpty());     
-     } }
+     } 
+}
     
