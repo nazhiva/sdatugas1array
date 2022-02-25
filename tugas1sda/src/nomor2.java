829021bb42ba4326bc672ff33fd5b1f2d43439ac
Nazhiva <100433664+nazhiva@users.noreply.github.com>
2022-02-25 09:12:56 -0800

    Update nomor2.java

--- tugas1sda/src/nomor2.java
+++ tugas1sda/src/nomor2.java
@@ -1,11 +1,10 @@
-public class nomor2 {
-    
+//Soal no 2
+//Tentukan hasil yang didapatkan ketika dilakukan perintah berikut ini:
+//size() -> berguna untuk menentukan besar atau jumlah karakter dalam array
+
+public class nomor2 {   
 public static void main(String[] args) {
-    String[] namaku = {"I","T","R","I"};
-    //Soal no 2
-    //Tentukan hasil yang didapatkan ketika dilakukan perintah berikut ini:
-    //size() -> berguna untuk menentukan besar atau jumlah karakter dalam array
-    System.out.println("ukuran arraynya adalah " + namaku.length);     
-    
-}
+    String[] namaku = {"I","T","R","I"};    
+    System.out.println("ukuran arraynya adalah " + namaku.length);   
+    }
 }
