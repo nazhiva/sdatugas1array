ffc1c52b2dbe631b4a89730d3b7dbbcb2392065b
Nazhiva <100433664+nazhiva@users.noreply.github.com>
2022-02-25 09:15:03 -0800

    Update nomor4.java

--- tugas1sda/src/nomor4.java
+++ tugas1sda/src/nomor4.java
@@ -1,5 +1,8 @@
-import java.util.ArrayList;
+//Soal no 4
+//Tentukan hasil yang didapatkan ketika dilakukan perintah berikut ini:
+//indexOf(a), indexOf(c), indexOf(q) -> berguna untuk mencari posisi dari char yang di minta
 
+import java.util.ArrayList;
 public class nomor4 {
     public static void main(String[] args) {
     //String[] nama = {"I","T","R","I"};
@@ -8,15 +11,9 @@ public class nomor4 {
     namaku.add("T");
     namaku.add("R");
     namaku.add("I");  
-
-    //Soal no 4
-    //Tentukan hasil yang didapatkan ketika dilakukan perintah berikut ini:
-    //indexOf(a), indexOf(c), indexOf(q) -> berguna untuk mencari posisi dari char yang di minta
-
     System.out.println(namaku.indexOf("a")); // Hasil ERROR karena tidak ada char "a" didalam string
     System.out.println(namaku.indexOf("c")); // Hasil ERROR karena tidak ada char "c" didalam string
     System.out.println(namaku.indexOf("q")); // Hasil ERROR karena tidak ada char "q" didalam string
-
     }
 }
 
